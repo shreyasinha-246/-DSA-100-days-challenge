94bb06bc43e527617a0d663513b30cff2a31470f
Shreya Sinha <shreyasiiinha@gmail.com>
2026-03-15 15:08:09 +0530

    Update 100d q7.c

---
+++ 100d q7.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// Recursive function to find Fibonacci number
+int fib(int n)
+{
+    if(n == 0)
+        return 0;
+    else if(n == 1)
+        return 1;
+    else
+        return fib(n-1) + fib(n-2);
+}
+
+int main()
+{
+    int n;
+
+    scanf("%d", &n);
+
+    printf("%d", fib(n));
+
+    return 0;
+}
